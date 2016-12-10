0caff34729bd5c3eff87656dbc4335d0bcf77a43
Richard Voelker <richard.voelker.software@gmail.com>
2016-12-10 18:11:47 -0500

    Add a mocked WidgetDbSet class for testing.

---
+++ Widget/Widget.Domain/Abstract/ITaxRateRepository.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Widget.Domain.Abstract {
+    public interface ITaxRateRepository {
+        decimal CalculateTaxAmount(decimal subTotal, string stateAbbreviation);
+    }
+}
