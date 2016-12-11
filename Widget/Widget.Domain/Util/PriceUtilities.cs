5c68bf9925de233a533770c3715f926c03fd5940
Richard Voelker <richard.voelker.software@gmail.com>
2016-12-11 09:15:16 -0500

    Refactor Widget Index view to use partials and HtmlHelpers.

---
+++ Widget/Widget.Domain/Util/PriceUtilities.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Widget.Domain.Util {
+    /// <summary>
+    /// Utitlity methods for calculating prices.
+    /// </summary>
+    public static class PriceUtilities {
+        private static readonly decimal DISCOUNT_AMOUNT = 0.1M;
+
+        /// <summary>
+        /// Calculate and return the discounted price for the Widget.
+        /// </summary>
+        /// <param name="widget">The Widget to extend.</param>
+        /// <returns>The discounted price.</returns>
+        public static decimal GetDiscountedPrice(this Models.Widget widget) {
+            return widget.BasePrice * (1.0M - DISCOUNT_AMOUNT);
+        }
+    }
+}
