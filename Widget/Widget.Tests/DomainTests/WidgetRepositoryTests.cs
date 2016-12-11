4ff7add81c55e110f4d50b9dc5669dc6dc5b8e92
Richard Voelker <richard.voelker.software@gmail.com>
2016-12-10 19:51:37 -0500

    Add IWidgetRepository test for ListWidgets.

---
+++ Widget/Widget.Tests/DomainTests/WidgetRepositoryTests.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Widget.Domain.Abstract;
+using Xunit;
+
+namespace Widget.Tests.DomainTests {
+    public class WidgetRepositoryTests {
+        private IWidgetRepository repository;
+
+        public WidgetRepositoryTests() {
+            //repository = new EfWidgetRepository(new MockWidgetDbContext().MockContext.Object);
+        }
+
+        /// <summary>
+        /// Test that the repository can return a list of all widgets.
+        /// </summary>
+        [Fact]
+        public void Test_ListWidgets() {
+            List<Domain.Models.Widget> widgets = repository.ListWidgets();
+
+            Assert.Equal(3, widgets.Count);
+            Assert.Equal("Widget 1", widgets[0].Name);
+            Assert.Equal("Widget 2", widgets[1].Name);
+            Assert.Equal("Widget 3", widgets[2].Name);
+        }
+    }
+}
