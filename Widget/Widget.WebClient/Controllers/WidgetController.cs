c8e6d0bbd9dd5c5932f7f0fb871a1eaefcb220e1
Richard Voelker <richard.voelker.software@gmail.com>
2016-12-10 20:31:08 -0500

    Create views in WebClient project.

--- Widget/Widget.WebClient/Controllers/WidgetController.cs
+++ Widget/Widget.WebClient/Controllers/WidgetController.cs
@@ -3,12 +3,21 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Widget.Domain.Abstract;
 
 namespace Widget.WebClient.Controllers {
     /// <summary>
     /// The main controller for the application.
     /// </summary>
     public class WidgetController : Controller {
+        private IStateRepository stateRepository;
+        private IWidgetRepository widgetRepository;
+
+        public WidgetController(IStateRepository stateRepository, IWidgetRepository widgetRepository) {
+            this.stateRepository = stateRepository;
+            this.widgetRepository = widgetRepository;
+        }
+
         // GET /
         public ActionResult Index() {
             return View();
